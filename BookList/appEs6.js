b604a5245df293e41b2f540545178686c83464c1
TouqeerBinArbab <touqeerahmed1633@gmail.com>
2021-05-04 08:37:16 +0500

    ls usage

--- BookList/appEs6.js
+++ BookList/appEs6.js
@@ -14,8 +14,8 @@ class UI {
     // Insert cols
 
     row.innerHTML = `<td>${book.title}</td>
-    <td>${book.title}</td>
-    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
     <td><a href='#' class='delete'>X</a></td>`;
     list.appendChild(row);
   }
@@ -50,6 +50,52 @@ class UI {
   }
 }
 
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static displatBooks() {
+    const books = Store.getBooks();
+    books.forEach(function(book) {
+      const ui = new UI();
+      ui.addBookToList(book)
+    });
+  }
+
+  static addBook(item) {
+    const books = Store.getBooks();
+
+    books.push(item);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    
+    books.forEach(function(book, index) {
+      console.log(book.isbn)
+      if (book.isbn === isbn){
+          books.splice(index, 1)
+      }
+
+      localStorage.setItem('books', JSON.stringify(books))
+    });
+
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displatBooks())
+
 // Event Listeners for add book
 document.getElementById("book-form").addEventListener("submit", function (e) {
   const title = document.getElementById("title").value,
@@ -66,7 +112,10 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
   if (title === "" || author === "" || isbn === "") {
     ui.showAlert("Please fill all fields", "error");
   } else {
+    // Add book to list
     ui.addBookToList(book);
+    Store.addBook(book);
+
     ui.showAlert("Book Added", "success");
     ui.clearFields();
   }
@@ -80,6 +129,8 @@ document.getElementById("book-list").addEventListener("click", function (e) {
 
   //delete book
   ui.deleteBook(e.target);
+
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
   //show message
   ui.showAlert("Book Removed!", "success");
